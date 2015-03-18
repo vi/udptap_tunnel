e021e1c33445b6e7e6f9457908fb3f42eca838f7
Vitaly _Vi Shukela <vi0oss@gmail.com>
2015-03-18 19:07:53 +0300

    seqpackettool: Implement SCTP

--- seqpackettool.c
+++ seqpackettool.c
@@ -5,104 +5,28 @@
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <arpa/inet.h>
 #include <errno.h>
 #include <signal.h>
 
 enum PartType {
-    UNIX_LISTEN = 1,
-    UNIX_CONNECT = 2,
-    SCTP4_LISTEN = 3,
-    SCTP4_CONNECT = 4,
-    SCTP6_LISTEN = 5,
-    SCTP6_CONNECT = 6,
+    LISTEN = 1,
+    CONNECT = 2,
     STDIO = 100,
     START = 101
 };
 
 struct Part {
     enum PartType type;
+    
+    int family;
+    int socktype;
+    int proto;
+    
     const char** arguments;
     int fd_recv;
     int fd_send;
 };
-
-static int parse_part(const char*** argv_cursor, struct Part *p) {
-    const char ***c = argv_cursor;
-    if (!**c) {
-        fprintf(stderr, "End of command line arguments: expected part type\n");
-        return -1;
-    }
-    
-    enum PartType t;
-    if (!strcmp(**c, "listen_unix") || !strcmp(**c, "unix_listen")) {
-        t = UNIX_LISTEN;
-    } else
-    if (!strcmp(**c, "connect_unix") || !strcmp(**c, "unix_connect")) {
-        t = UNIX_CONNECT;
-    } else
-    if (!strcmp(**c, "sctp_listen4") || !strcmp(**c, "sctp4_listen") || !strcmp(**c, "listen_sctp4")) {
-        t = SCTP4_LISTEN;
-    } else
-    if (!strcmp(**c, "sctp_connect4") || !strcmp(**c, "sctp4_connect") || !strcmp(**c, "connect_sctp4")) {
-        t = SCTP4_CONNECT;
-    } else
-    if (!strcmp(**c, "sctp_listen6") || !strcmp(**c, "sctp6_listen") || !strcmp(**c, "listen_sctp6")) {
-       t = SCTP6_LISTEN;
-    } else
-    if (!strcmp(**c, "sctp_connect6") || !strcmp(**c, "sctp6_connect") || !strcmp(**c, "connect_sctp6")) {
-        t = SCTP6_CONNECT;
-    } else
-    if (!strcmp(**c, "start") || !strcmp(**c, "exec")) {
-        t = START;
-    } else
-    if (!strcmp(**c, "-")) {
-        t = STDIO;
-    } else {
-        fprintf(stderr, "Part type %s is incorrect, look for --help message\n", **c);
-        return -1;
-    }
-    ++*c;
-    
-    p->type = t;
-    p->fd_recv = -1;
-    p->fd_send = -1;
-    
-    if      (t==STDIO) { p->arguments = NULL; }
-    else if (t==UNIX_LISTEN || t==UNIX_CONNECT) {
-        if (!**c) { fprintf(stderr, "After listen_unix or connect_unix one more argument is expected\n"); return -1; }
-        p->arguments = (const char**) malloc(sizeof(const char*) * 1);
-        p->arguments[0] = **c; ++*c;
-    }
-    else if (t==SCTP4_LISTEN || t==SCTP4_CONNECT || t==SCTP6_LISTEN || t==SCTP6_CONNECT) {
-        if (!**c || !*(*c+1) ) { fprintf(stderr, "After sctp_listen or connect two more arguments are expected\n"); return -1; }
-        p->arguments = (const char**) malloc(sizeof(const char*) * 2);
-        p->arguments[0] = **c; ++*c;
-        p->arguments[1] = **c; ++*c;
-    }
-    else if (t==START) {
-        if (!**c || !*(*c+1) || !*(*c+2) ) { fprintf(stderr, "After start more arguments are expected\n"); return -1; }
-        const char* delim = **c; ++*c;
-        // delim is supposed to be or more dashes, but let's be more flexible and just not check for the delimiter content at all
-        int n;
-        for(n=0; ;++n) {
-            if (!*(*c+n)) { fprintf(stderr, "Premature end of command line arguments. Delimiter %s is expected.\n", delim); return -1; }
-            if (!strcmp(*(*c+n), delim)) break;
-        }
-        if (n==0) { fprintf(stderr, "Empty list of child command line arguments\n"); return -1; }
-        p->arguments = (const char**) malloc(sizeof(const char*) * (n + 1));
-        
-        int i;
-        for (i=0; i<n; ++i) {
-            p->arguments[i] = **c; ++*c;
-        }
-        p->arguments[n] = NULL;
-        ++*c; // skip trailing delimiter
-    } else {
-        fprintf(stderr, "Assertion failed 1\n");
-        return -1;
-    }
-    return 0;
-}
 
 static void free_part(struct Part *p) {
     if (p->fd_recv == p->fd_send) {
@@ -116,37 +40,80 @@ static void free_part(struct Part *p) {
 
 
 #ifndef UNIX_PATH_MAX
-#define UNIX_PATH_MAX 108
+#define UNIX_PATH_MAX 104
 #endif 
 
-static int init_part_unix(struct Part *p, int listen_once) {
+static int init_addr(struct Part *p, struct sockaddr_storage *s, socklen_t *len)
+{
+    switch (p->family) {
+        case AF_UNIX: {
+            struct sockaddr_un *sa = (struct sockaddr_un*) s;
+            
+            *len = sizeof(*sa);
+            memset(sa, 0, *len);
+            sa->sun_family = AF_UNIX;
+                
+            snprintf(sa->sun_path, UNIX_PATH_MAX, "%s", p->arguments[0]);
+            if(sa->sun_path[0]=='@')sa->sun_path[0]=0; // abstract socket address; man 7 unix
+        
+            return 0;
+        }
+        case AF_INET: {
+            struct sockaddr_in *sa = (struct sockaddr_in*) s;
+            
+            *len = sizeof(*sa);
+            memset(sa, 0, *len);
+            sa->sin_family = AF_INET;
+            sa->sin_port = htons(atoi(p->arguments[1]));
+            int ret = inet_pton(AF_INET, p->arguments[0], &sa->sin_addr);
+            
+            if (ret!=1) {perror("inet_pton"); return -1; }
+            return 0;
+        }
+        case AF_INET6: {
+            struct sockaddr_in6 *sa = (struct sockaddr_in6*) s;
+            
+            *len = sizeof(*sa);
+            memset(sa, 0, *len);
+            sa->sin6_family = AF_INET6;
+            sa->sin6_port = htons(atoi(p->arguments[1]));
+            int ret = inet_pton(AF_INET6, p->arguments[0], &sa->sin6_addr);
+            
+            if (ret!=1) {perror("inet_pton"); return -1; }
+            return 0;
+        }
+        default:
+            fprintf(stderr, "Assertion failed 6\n");
+            return -1;
+    }
+}
+
+static int init_part_socket(struct Part *p, int listen_once) {
     int sso;
-    struct sockaddr_un sa;
+    struct sockaddr_storage sa;
+    socklen_t salen;
     int ret;
     
-    sso = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+    sso = socket(p->family, p->socktype, p->proto);
     if (sso == -1) { perror("socket"); return -1; }
     
-    memset(&sa, 0, sizeof sa);
-    sa.sun_family = AF_UNIX;
-        
-    snprintf(sa.sun_path, UNIX_PATH_MAX, "%s", p->arguments[0]);
-    if(sa.sun_path[0]=='@')sa.sun_path[0]=0; // abstract socket address; man 7 unix
+    ret = init_addr(p, &sa, &salen); if (ret == -1) return -1;
     
-    if (p->type == UNIX_CONNECT) {
-        ret = connect(sso, (struct sockaddr*)&sa, sizeof sa);
+    if (p->type == CONNECT) {
+        ret = connect(sso, (struct sockaddr*)&sa, salen);
         if (ret==-1) { close(sso); perror("connect"); return -1; }
         
         p->fd_send = sso;
         p->fd_recv = sso;
         return 0;
     } else 
-    if (p->type == UNIX_LISTEN) {
-        struct sockaddr_un sa2;
-        socklen_t l = sizeof sa2;
+    if (p->type == LISTEN) {
+        struct sockaddr_storage sa2;
+        socklen_t l = salen;
         int cso;
+        memset(&sa2, 0, sizeof sa2);
         
-        ret = bind(sso, (struct sockaddr*)&sa, sizeof sa);
+        ret = bind(sso, (struct sockaddr*)&sa, salen);
         if (ret==-1) { close(sso); perror("bind"); return -1; }
             
         if (listen(sso, listen_once?1:16)==-1) { close(sso); perror("listen"); return -1; }
@@ -163,7 +130,7 @@ static int init_part_unix(struct Part *p, int listen_once) {
         } else {
             signal(SIGCHLD, SIG_IGN);
             for(;;) {
-                cso  = accept(sso, (struct sockaddr*)&sa2,&l);
+                cso  = accept(sso, (struct sockaddr*)&sa2, &l);
                 
                 pid_t child = fork();
                 if (child == -1) { close(cso); perror("fork"); usleep(100000); continue; }
@@ -184,10 +151,7 @@ static int init_part_unix(struct Part *p, int listen_once) {
         return -1;
     }
 }
-static int init_part_sctp(struct Part *p, int listen_once) {
-    fprintf(stderr, "SCTP is not implemented\n");
-    return -1;
-}
+
 extern char ** environ;
 static int init_part_subprocess(struct Part *p, struct Part *first_part) {
     int ret;
@@ -226,14 +190,9 @@ static int init_part_subprocess(struct Part *p, struct Part *first_part) {
     
 static int init_part(struct Part *p, int listen_once, struct Part *first_part) {
     switch(p->type) {
-        case UNIX_LISTEN:
-        case UNIX_CONNECT:
-            return init_part_unix(p, listen_once);
-        case SCTP4_LISTEN:
-        case SCTP4_CONNECT:
-        case SCTP6_LISTEN:
-        case SCTP6_CONNECT:
-            return init_part_sctp(p, listen_once);
+        case LISTEN:
+        case CONNECT:
+            return init_part_socket(p, listen_once);
         case START:
             return init_part_subprocess(p, first_part);
         case STDIO:
@@ -277,7 +236,113 @@ static void exchange_data(struct Part* part1, struct Part* part2, char* buffer, 
         }
     }
 }
+
+
+static int parse_part(const char*** argv_cursor, struct Part *p) {
+    const char ***c = argv_cursor;
+    if (!**c) {
+        fprintf(stderr, "End of command line arguments: expected part type\n");
+        return -1;
+    }
     
+    enum PartType t;
+    int family = 0;
+    int socktype = 0;
+    int proto = 0;
+    if (!strcmp(**c, "listen_unix") || !strcmp(**c, "unix_listen")) {
+        t = LISTEN;
+        family = AF_UNIX;
+        socktype = SOCK_SEQPACKET;
+    } else
+    if (!strcmp(**c, "connect_unix") || !strcmp(**c, "unix_connect")) {
+        t = CONNECT;
+        family = AF_UNIX;
+        socktype = SOCK_SEQPACKET;
+    } else
+    if (!strcmp(**c, "sctp_listen4") || !strcmp(**c, "sctp4_listen") || !strcmp(**c, "listen_sctp4")) {
+        t = LISTEN;
+        family = AF_INET;
+        socktype = SOCK_STREAM;
+        proto = IPPROTO_SCTP;
+    } else
+    if (!strcmp(**c, "sctp_connect4") || !strcmp(**c, "sctp4_connect") || !strcmp(**c, "connect_sctp4")) {
+        t = CONNECT;
+        family = AF_INET;
+        socktype = SOCK_STREAM;
+        proto = IPPROTO_SCTP;
+    } else
+    if (!strcmp(**c, "sctp_listen6") || !strcmp(**c, "sctp6_listen") || !strcmp(**c, "listen_sctp6")) {
+        t = LISTEN;
+        family = AF_INET6;
+        socktype = SOCK_STREAM;
+        proto = IPPROTO_SCTP;
+    } else
+    if (!strcmp(**c, "sctp_connect6") || !strcmp(**c, "sctp6_connect") || !strcmp(**c, "connect_sctp6")) {
+        t = CONNECT;
+        family = AF_INET6;
+        socktype = SOCK_STREAM;
+        proto = IPPROTO_SCTP;
+    } else
+    if (!strcmp(**c, "start") || !strcmp(**c, "exec")) {
+        t = START;
+    } else
+    if (!strcmp(**c, "-")) {
+        t = STDIO;
+    } else {
+        fprintf(stderr, "Part type %s is incorrect, look for --help message\n", **c);
+        return -1;
+    }
+    ++*c;
+    
+    p->type = t;
+    p->family = family;
+    p->socktype = socktype;
+    p->proto = proto;
+    p->fd_recv = -1;
+    p->fd_send = -1;
+    
+    if      (t==STDIO) { p->arguments = NULL; }
+    else if (t==LISTEN || t==CONNECT) {
+        if (family == AF_UNIX) {
+            if (!**c) { fprintf(stderr, "After listen_unix or connect_unix one more argument is expected\n"); return -1; }
+            p->arguments = (const char**) malloc(sizeof(const char*) * 1);
+            p->arguments[0] = **c; ++*c;
+        }
+        else if (family == AF_INET || family == AF_INET6) {
+            if (!**c || !*(*c+1) ) { fprintf(stderr, "After sctp_listen or connect two more arguments are expected\n"); return -1; }
+            p->arguments = (const char**) malloc(sizeof(const char*) * 2);
+            p->arguments[0] = **c; ++*c;
+            p->arguments[1] = **c; ++*c;
+        } else {
+            fprintf(stderr, "Assertion failed 4\n"); return -1;
+        }
+    }
+    else if (t==START) {
+        if (!**c || !*(*c+1) || !*(*c+2) ) { fprintf(stderr, "After start more arguments are expected\n"); return -1; }
+        const char* delim = **c; ++*c;
+        // delim is supposed to be or more dashes, but let's be more flexible and just not check for the delimiter content at all
+        int n;
+        for(n=0; ;++n) {
+            if (!*(*c+n)) { fprintf(stderr, "Premature end of command line arguments. Delimiter %s is expected.\n", delim); return -1; }
+            if (!strcmp(*(*c+n), delim)) break;
+        }
+        if (n==0) { fprintf(stderr, "Empty list of child command line arguments\n"); return -1; }
+        p->arguments = (const char**) malloc(sizeof(const char*) * (n + 1));
+        
+        int i;
+        for (i=0; i<n; ++i) {
+            p->arguments[i] = **c; ++*c;
+        }
+        p->arguments[n] = NULL;
+        ++*c; // skip trailing delimiter
+    } else {
+        fprintf(stderr, "Assertion failed 1\n");
+        return -1;
+    }
+    return 0;
+}
+
+
 int main(int argc, const char* argv[]) {
     if (argc==1 || !strcmp(argv[1], "--help")) {
         fprintf(stderr, "Usage: seqpackettool [options] part part\n");
